e435b677f8214712668cd9242465150c81ef6a88
rackaracka123 <55146597+rackaracka123@users.noreply.github.com>
2022-10-01 21:31:15 +0200

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MAULAREN :))
+          Testing testing
         </p>
       </header>
     </div>
